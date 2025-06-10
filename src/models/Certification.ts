d67c718638a669593e62ab61eabecddd04d0c4d4
insertusernamed <113151220+insertusernamed@users.noreply.github.com>
2025-06-10 17:07:20 -0400

    updated certification type

--- src/models/Certification.ts
+++ src/models/Certification.ts
@@ -1,5 +1,11 @@
+export interface MediaItem {
+    type: string;
+    url: string;
+    caption: string;
+}
+
 export interface Certification {
     id: string | number;
     name: string;
-    media: string;
+    media: MediaItem[];
 }
